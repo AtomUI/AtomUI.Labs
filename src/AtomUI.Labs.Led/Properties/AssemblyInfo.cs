060681152446b23de1c1f8dacd25b32df5b96960
youname <youremail>
2026-07-20 15:15:58 +0800

    add Led main project

---
+++ src/AtomUI.Labs.Led/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Avalonia.Metadata;
+
+[assembly: XmlnsPrefix("https://atomui.net/labs", "labs")]
+[assembly: XmlnsDefinition("https://atomui.net/labs", "AtomUI.Labs.Led")]
+[assembly: XmlnsDefinition("https://atomui.net/labs", "AtomUI.Labs.Led.Segment")]
+[assembly: XmlnsDefinition("https://atomui.net/labs", "AtomUI.Labs.Led.Matrix")]
